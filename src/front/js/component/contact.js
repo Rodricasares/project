b0272de2069e12890eb6e82e3b8ed62f47a9b190
Rodricasares <rodrigocantudo@gmail.com>
2022-11-08 12:56:26 +0000

    boton task & fuente

--- src/front/js/component/contact.js
+++ src/front/js/component/contact.js
@@ -26,13 +26,16 @@ export const Contact = () => (
             <h1 className="title  fw-bold mb-3">Contactar</h1>
             <div className="col-md-6 offset-md-3 text-secondary">
               <div className="input-group mb-3">
-                <span className="input-group-text" id="basic-addon1">
+                <span
+                  className="input-group-text border border-0 bg-transparent"
+                  id="basic-addon1"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
+                    width="40"
+                    height="40"
                     fill="currentColor"
-                    className="bi bi-people"
+                    className="bi bi-people iconContact"
                     viewBox="0 0 16 16"
                   >
                     <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
@@ -41,21 +44,25 @@ export const Contact = () => (
                 <input
                   type="text"
                   name="user"
-                  className="form-control"
+                  className="form-control border border-0  bg-transparent formInput"
                   placeholder="Username"
                   aria-label="Username"
                   aria-describedby="basic-addon1"
+                  required
                 />
               </div>
 
               <div className="input-group mb-3">
-                <span className="input-group-text" id="basic-addon1">
+                <span
+                  className="input-group-text border-0 bg-transparent iconContact"
+                  id="basic-addon1"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
+                    width="40"
+                    height="40"
                     fill="currentColor"
-                    className="bi bi-envelope-check"
+                    className="bi bi-envelope-check "
                     viewBox="0 0 16 16"
                   >
                     <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z" />
@@ -65,21 +72,25 @@ export const Contact = () => (
                 <input
                   type="text"
                   name="email"
-                  className="form-control"
+                  className="form-control border-0 bg-transparent formInput"
                   placeholder="Email"
                   aria-label="Email"
                   aria-describedby="basic-addon1"
+                  required
                 />
               </div>
 
               <div className="input-group mb-3">
-                <span className="input-group-text" id="basic-addon1">
+                <span
+                  className="input-group-text border-0 bg-transparent iconContact"
+                  id="basic-addon1"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
+                    width="40"
+                    height="40"
                     fill="currentColor"
-                    className="bi bi-telephone"
+                    className="bi bi-telephone "
                     viewBox="0 0 16 16"
                   >
                     <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
@@ -88,18 +99,18 @@ export const Contact = () => (
                 <input
                   type="text"
                   name="phone"
-                  className="form-control"
+                  className="form-control border-0 bg-transparent formInput"
                   placeholder="Phone"
                   aria-label="Phone"
                   aria-describedby="basic-addon1"
                 />
 
                 <div className="input-group mt-3">
-                  <span className="input-group-text">
+                  <span className="input-group-text border-0 bg-transparent iconContact">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
+                      width="40"
+                      height="40"
                       fill="currentColor"
                       className="bi bi-card-text"
                       viewBox="0 0 16 16"
@@ -109,17 +120,32 @@ export const Contact = () => (
                     </svg>
                   </span>
                   <textarea
-                    className="form-control"
+                    className="form-control border-0 bg-transparent formInput"
                     name="text-area"
                     aria-label="With textarea"
+                    placeholder="Text"
+                    aria-describedby="basic-addon1"
                   ></textarea>
                 </div>
-                <button type="submit" className="btn btn-outline-warning mt-3">
-                  Info
-                </button>
               </div>
             </div>
           </form>
+          <div class="col-md-6 offset-md-3 my-3">
+            {" "}
+            <button type="submit" className="btn btn-dark mt-3 float-end">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-send me-2"
+                viewBox="0 0 16 16"
+              >
+                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+              </svg>
+              Enviar
+            </button>
+          </div>
         </div>
       </div>
     </div>
