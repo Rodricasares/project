397711b8dbfac8069607b82378aebde91da12c9a
Rodricasares <rodrigocantudo@gmail.com>
2022-11-23 10:45:15 +0000

    more info, resp.

--- src/front/js/component/contact.js
+++ src/front/js/component/contact.js
@@ -17,12 +17,12 @@ export const Contact = () => (
           >
             <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
           </svg>
- {/*FORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORM*/}
-          
+          {/*FORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORMFORM**FORM*/}
 
- <form target="_blank" 
-          action="https://formsubmit.co/4dec7747a969da51e171c170dafdf32f"  method="POST"
-          >                             
+
+          <form target="_blank"
+            action="https://formsubmit.co/4dec7747a969da51e171c170dafdf32f" method="POST"
+          >
             {" "}
             <h1 className="title  fw-bold mb-3">Contactar</h1>
             <div className="col-md-6 offset-md-3 text-secondary">
@@ -42,7 +42,7 @@ export const Contact = () => (
                     <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
                   </svg>
                 </span>
-                  <input
+                <input
                   type="text"
                   name="name"
                   className="form-control  bg-transparent formInput"
@@ -51,14 +51,14 @@ export const Contact = () => (
                   aria-describedby="basic-addon1"
                   required
                 />
-             
+
               </div>
-             
-                <div className="input-group">
-                    <span
-                    className="input-group-text border-0 bg-transparent iconContact"
-                   id="basic-addon1"                >
-                   <svg
+
+              <div className="input-group">
+                <span
+                  className="input-group-text border-0 bg-transparent iconContact"
+                  id="basic-addon1"                >
+                  <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="40"
                     height="40"
@@ -70,7 +70,7 @@ export const Contact = () => (
                     <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z" />
                   </svg>
                 </span>
-                
+
                 <input
                   type="email"
                   name="email"
@@ -108,102 +108,102 @@ export const Contact = () => (
                   aria-label="Phone"
                   aria-describedby="basic-addon1"
                 />
- </div>
+              </div>
 
-                <div className="input-group mt-2">
-                  <span className="input-group-text border-0 bg-transparent iconContact">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="40"
-                      height="40"
-                      fill="currentColor"
-                      className="bi bi-card-text"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
-                      <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
-                    </svg>
-                  </span>
-                  <textarea placeholder="Your Message" className="form-control  bg-transparent formInput" name="message"  required></textarea>
+              <div className="input-group mt-2">
+                <span className="input-group-text border-0 bg-transparent iconContact">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="40"
+                    height="40"
+                    fill="currentColor"
+                    className="bi bi-card-text"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+                    <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
+                  </svg>
+                </span>
+                <textarea placeholder="Your Message" className="form-control  bg-transparent formInput" name="message" required></textarea>
 
-                </div>
+              </div>
 
 
             </div>
             <div className="col-md-6 offset-md-3 mt-3">
-                     <button type="submit" className="btn btn-dark mt-3 float-end">
+              <button type="submit" className="btn btn-dark mt-3 float-end">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-send me-2"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                </svg>
+                Enviar
+              </button>
+            </div>
+          </form>
+
+          {/*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*/}
+
+          <div className="col-md-6 offset-md-3 d-flex justify-content-around socialButton rounded-pill p-3 bg-dark my-5">
+            <h4 className="text-light mt-2">Info de interés:</h4>
+            <a
+              type="button"
+              className="btn btn-danger  rounded-circle"
+              href="https://drive.google.com/file/d/1wHJ811HHx_J-83zxSEggyuZNLvo5ofgR/view?usp=sharing"
+              download="CvRodrigo"
+              target="_blank"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-arrow-down" viewBox="0 0 16 16">
+                <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293V6.5z" />
+                <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z" />
+              </svg>
+            </a>
+
+            <a
+              type="button"
+              className="btn btn-info  rounded-circle"
+              href="https://www.linkedin.com/in/rodrigo-cantudo/"
+              target="_blank"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
                 fill="currentColor"
-                className="bi bi-send me-2"
+                className="bi bi-linkedin"
                 viewBox="0 0 16 16"
               >
-                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
               </svg>
-              Enviar
-            </button>
-            </div>
-          </form>
+            </a>
+            <a
+              className="btn btn-dark rounded-circle"
+              href="https://github.com/Rodricasares"
+              target="_blank"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-github"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+              </svg>
+            </a></div>
 
-{/*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*BUTTONS*/}
-
-         <div className="col-md-6 offset-md-3 d-flex justify-content-around socialButton rounded-pill bg-dark my-5">
-         <h4 className="text-light float-start  mt-4">Info de interés:</h4>       
-                            <a
-        type="button"
-        className="btn btn-danger m-4 rounded-circle"
-        href="https://drive.google.com/file/d/1wHJ811HHx_J-83zxSEggyuZNLvo5ofgR/view?usp=sharing"
-        download="CvRodrigo"
-        target="_blank"
-      >
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-arrow-down" viewBox="0 0 16 16">
-  <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293V6.5z"/>
-  <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2zM9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5v2z"/>
-</svg>
-      </a>
-
-      <a
-        type="button"
-        className="btn btn-info m-4 rounded-circle"
-        href="https://www.linkedin.com/in/rodrigo-cantudo/"
-        target="_blank"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-linkedin"
-          viewBox="0 0 16 16"
-        >
-          <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
-        </svg>
-      </a>
-      <a
-        className="btn btn-dark m-4 rounded-circle"
-        href="https://github.com/Rodricasares"
-        target="_blank"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-github"
-          viewBox="0 0 16 16"
-        >
-          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-        </svg>
-      </a></div>
-
+        </div>
       </div>
+    </div>
   </div>
-</div>
-</div>
 
-   
- 
+
+
 
 );
